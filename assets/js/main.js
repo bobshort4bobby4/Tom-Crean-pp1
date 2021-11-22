6080e4e156693e253cd42def814af3d4d14ad411
bobshort4bobby4 <treborken@gmail.com>
2021-11-22 20:45:14 +0000

    FIX removed commented out code js files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,5 @@
 window.onload = function () {
-	//window.addEventListener('scroll', function (e) {
-	//	if (window.pageYOffset > 100) {
-	//		document.querySelector("header").classList.add('is-scrolling');
-	//	} else {
-	//		document.querySelector("header").classList.remove('is-scrolling');
-	//	}
-	//})//;
+
 
 	const menu_btn = document.querySelector('.hamburger');
 	const mobile_menu = document.querySelector('.mobile-nav');
@@ -16,33 +10,3 @@ window.onload = function () {
 	});
 
 }
-/*	var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-//function showSlides(n) {
-/*  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].setAttribute("style", "display:block;");
-  //slides[slideIndex-1].style.display = "block";
-  //dots[slideIndex-1].className += " active";
-}
-var boxElement = document.getElementsByClassName('box') */
